ad8a513ecf5d92c54c8f53d5ec2eed1bf7a52136
Yuhang Zhao <2546789017@qq.com>
2022-05-22 15:10:49 +0800

    wip
    Signed-off-by: Yuhang Zhao <2546789017@qq.com>

---
+++ build/msvc-static.bat
@@ -0,0 +1,37 @@
+:: MIT License
+::
+:: Copyright (C) 2022 by wangwenx190 (Yuhang Zhao)
+::
+:: Permission is hereby granted, free of charge, to any person obtaining a copy
+:: of this software and associated documentation files (the "Software"), to deal
+:: in the Software without restriction, including without limitation the rights
+:: to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+:: copies of the Software, and to permit persons to whom the Software is
+:: furnished to do so, subject to the following conditions:
+::
+:: The above copyright notice and this permission notice shall be included in
+:: all copies or substantial portions of the Software.
+::
+:: THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+:: IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+:: FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+:: AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+:: LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+:: OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+:: SOFTWARE.
+
+@echo off
+setlocal
+call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+cd /d "%~dp0"
+if exist build rd /s /q build
+md build
+cd build
+set _vcpkg_dir=C:\Develop\Sources\build-qt-dev\vcpkg
+set _qt_dir=C:\Develop\Sources\build-qt-dev\build\windows\msvc_x64_static_release
+cmake -DVCPKG_TARGET_TRIPLET=x64-windows-static -DCMAKE_TOOLCHAIN_FILE="%_vcpkg_dir%\scripts\buildsystems\vcpkg.cmake" -DCMAKE_PREFIX_PATH="%_qt_dir%" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INTERPROCEDURAL_OPTIMIZATION_RELEASE=ON -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -GNinja "%~dp0.."
+cmake --build . --target all --config Release --parallel
+endlocal
+cd /d "%~dp0"
+pause
+exit /b
